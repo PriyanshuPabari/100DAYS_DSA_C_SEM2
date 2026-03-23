7086383038d4061ac11aa97f996eed585cf90c5c
PriyanshuPabari <priyanshupabari007@gmail.com>
2026-03-24 01:23:10 +0530

    Add day55.c, day55_ques2.c

---
+++ day55_ques2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+// Day 55 - Graph - Adjacency Matrix
+// Additional practice
+
+int main() {
+    printf("Day 55 - Practice: Graph - Adjacency Matrix\n");
+    
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", i * i);
+    }
+    printf("\n");
+    
+    return 0;
+}
