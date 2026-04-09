f5566560404fa8deece4008c0227b3db4886282f
PriyanshuPabari <priyanshupabari007@gmail.com>
2026-04-09 23:26:59 +0530

    Add day74.c, day74_ques2.c

---
+++ day74_ques2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+// Day 74 - DP - Rod Cutting
+// Additional practice
+
+int main() {
+    printf("Day 74 - Practice: DP - Rod Cutting\n");
+    
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", i * i);
+    }
+    printf("\n");
+    
+    return 0;
+}
