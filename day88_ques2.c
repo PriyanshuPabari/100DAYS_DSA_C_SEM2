fea57d61146a6bb838f9eb2e57c04e0eb917cc88
PriyanshuPabari <priyanshupabari007@gmail.com>
2026-04-23 01:44:18 +0530

    Add day88.c, day88_ques2.c

---
+++ day88_ques2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+// Day 88 - Trie - Insert and Search
+// Additional practice
+
+int main() {
+    printf("Day 88 - Practice: Trie - Insert and Search\n");
+    
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", i * i);
+    }
+    printf("\n");
+    
+    return 0;
+}
