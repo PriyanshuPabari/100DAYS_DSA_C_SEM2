01c5d7a5c37ba985cc1c62e146350cf9bcd1fd10
PriyanshuPabari <priyanshupabari007@gmail.com>
2026-04-24 01:45:28 +0530

    Add day90.c, day90_ques2.c

---
+++ day90_ques2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+// Day 90 - Segment Tree - Range Query
+// Additional practice
+
+int main() {
+    printf("Day 90 - Practice: Segment Tree - Range Query\n");
+    
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", i * i);
+    }
+    printf("\n");
+    
+    return 0;
+}
