a0310fde13addda17ce2dbb3d318d94d81a180ca
PriyanshuPabari <priyanshupabari007@gmail.com>
2026-04-27 01:45:31 +0530

    Add day93.c, day93_ques2.c

---
+++ day93_ques2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+// Day 93 - Advanced - KMP Algorithm
+// Additional practice
+
+int main() {
+    printf("Day 93 - Practice: Advanced - KMP Algorithm\n");
+    
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", i * i);
+    }
+    printf("\n");
+    
+    return 0;
+}
