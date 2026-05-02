4136cb6113d283c879da29a78c23045fe09b4479
PriyanshuPabari <priyanshupabari007@gmail.com>
2026-05-03 01:17:20 +0530

    Add day99.c, day99_ques2.c

---
+++ day99_ques2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+// Day 99 - Advanced - AVL Tree
+// Additional practice
+
+int main() {
+    printf("Day 99 - Practice: Advanced - AVL Tree\n");
+    
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", i * i);
+    }
+    printf("\n");
+    
+    return 0;
+}
