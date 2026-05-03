436eca47752d3c6e3f8b81bc35a8acf31e125f80
PriyanshuPabari <priyanshupabari007@gmail.com>
2026-05-03 23:15:14 +0530

    Add day100.c, day100_ques2.c

---
+++ day100.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Day 100 - Advanced - Red Black Tree
+// Practice problem
+
+int main() {
+    printf("Day 100: Advanced - Red Black Tree\n");
+    
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
